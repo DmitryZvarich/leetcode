d0aef8f4cadfdedcca4baa26180a8756d82e77ec
dzvarych <dmitry.zvarich@gmail.com>
2023-03-23 15:58:26 -0700

    208. Implement Trie (Prefix Tree)

---
+++ src/main/java/org/example/trie/Trie.java
@@ -0,0 +1,44 @@
+package org.example.trie;
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (!node.containsKey(c)) {
+                node.put(c, new TrieNode());
+            }
+            node = node.get(c);
+        }
+
+        node.setEnd();
+    }
+
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+
+        for (char c : word.toCharArray()) {
+            node = node.get(c);
+            if (node == null) {
+                return null;
+            }
+        }
+
+        return node;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+}
