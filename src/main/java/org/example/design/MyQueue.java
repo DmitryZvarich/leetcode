b70d75b0019554a547e0a91f3f242e3c787e2b2f
dzvarych <dmitry.zvarich@gmail.com>
2023-03-24 16:01:58 -0700

    232. Implement Queue using Stacks

---
+++ src/main/java/org/example/design/MyQueue.java
@@ -0,0 +1,34 @@
+package org.example.design;
+
+import java.util.Stack;
+
+public class MyQueue {
+    private final Stack<Integer> stack1;
+    private final Stack<Integer> stack2;
+
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        while (!stack1.isEmpty())
+            stack2.push(stack1.pop());
+        stack2.push(x);
+        while (!stack2.isEmpty())
+            stack1.push(stack2.pop());
+    }
+
+    public int pop() {
+        return stack1.pop();
+    }
+
+    public int peek() {
+        return stack1.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+}
