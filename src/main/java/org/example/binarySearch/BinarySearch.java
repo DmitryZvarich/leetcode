a3ed75118a67a9c3f129df820b86b3b6ff2bc4de
dzvarych <dmitry.zvarich@gmail.com>
2023-04-03 12:11:32 -0700

    704. Binary Search

---
+++ src/main/java/org/example/binarySearch/BinarySearch.java
@@ -0,0 +1,22 @@
+package org.example.binarySearch;
+
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > target) {
+                right = mid;
+            } else if (nums[mid] < target){
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
